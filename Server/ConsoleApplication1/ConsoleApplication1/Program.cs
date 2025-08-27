34abc1df159776d4d9863d38b35f606839d1867b
Sharmin Motameni <s.motameni@kandaidea.com>
2025-08-27 20:40:59 +0330

    Add two side project

---
+++ Server/ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+public class SimpleTcpSrvr
+{
+    public static void Main()
+    {
+        try
+        {
+            int recv;
+            byte[] data = new byte[1024];
+            IPEndPoint ipep = new IPEndPoint(IPAddress.Any, 9050);
+
+            Socket newsock = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+            newsock.Bind(ipep);
+            newsock.Listen(10);
+            Console.WriteLine("Waiting for a client to connect...");
+            Console.WriteLine("Port is 9050 and ip is your pc ip...");
+            Socket client = newsock.Accept();
+            IPEndPoint clientep = (IPEndPoint)client.RemoteEndPoint;
+            Console.WriteLine("Connected with {0} at port {1}", clientep.Address, clientep.Port);
+
+            string welcome = "Welcome to my test server";
+            data = Encoding.ASCII.GetBytes(welcome);
+            client.Send(data, data.Length, SocketFlags.None);
+
+            while (true)
+            {
+                data = new byte[1024];
+                recv = client.Receive(data);
+                if (recv == 0)
+                    break;
+
+                Console.WriteLine(Encoding.ASCII.GetString(data, 0, recv));
+                client.Send(data, recv, SocketFlags.None);
+            }
+            Console.WriteLine("Disconnected from {0}", clientep.Address);
+            client.Close();
+            newsock.Close();
+        }
+        catch (SocketException e)
+        {
+            Console.WriteLine("Connection is closed.");
+        }
+    }
+}
---
+++ Server/ConsoleApplication1/ConsoleApplication1/ReadMe.txt
@@ -0,0 +1,11 @@
+﻿#This is a basic echo server example and is not optimized for multiple clients or production use.
+
+Originally built in 2014 for testing and learning purposes.
+
+To run locally:
+
+- Start the server first.
+
+- Then run the client.
+
+* If running on different machines, replace 127.0.0.1 in the client with the server’s IP address.
